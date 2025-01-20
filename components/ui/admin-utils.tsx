29aa997cdc5c08fe17152241984ad48da0fbd77d
kieloBash <kielo_bash_mercado@dlsu.edu.ph>
2025-01-20 15:47:49 +0800

    added floating admin utils

--- components/ui/admin-utils.tsx
+++ components/ui/admin-utils.tsx
@@ -1,6 +1,8 @@
 'use client'
 import {
     Eye,
+    HistoryIcon,
+    PlusIcon,
     Settings2Icon,
     UserPlus,
     Users,
@@ -21,54 +23,105 @@ import {
     DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { createAdminAccount } from "@/lib/admin-tools"
-import { toast } from "@/hooks/use-toast"
+
+import { handleCreateAdminAccount, handleCreateUserAccount, handleCreateManagmentAccount } from "../admin-tools/actions"
+import HistoryModal from "../admin-tools/modals/history-modal"
+import { useState } from "react"
+
+type SelectedType = "add-history" | "";
 
 export default function UiAdminUtils() {
-
-    async function handleCreateAdminAccount() {
-        toast({ description: "Please wait while we create an admin account" })
-        const res = await createAdminAccount({})
-        if (res?.success) {
-            toast({ description: `Email: admin@gmail.com Password:User1234!` })
-        } else {
-            toast({ description: res?.error, variant: "destructive" })
-        }
-    }
+    const [selected, setSelected] = useState<SelectedType>("");
 
     return (
-        <div className="fixed lg:bottom-10 bottom-36 mb-2 right-2">
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="icon">
-                        <Settings2Icon className="absolute h-[1.2rem] w-[1.2rem]" />
-                        <span className="sr-only">Toggle Admin Utils</span>
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                    <DropdownMenuLabel>Admin Utilities</DropdownMenuLabel>
-                    <DropdownMenuGroup>
-                        <DropdownMenuSub>
-                            <DropdownMenuSubTrigger>
-                                <Users />
-                                <span>User</span>
-                            </DropdownMenuSubTrigger>
-                            <DropdownMenuSubContent>
-                                <DropdownMenuItem onClick={handleCreateAdminAccount}>
-                                    <UserPlus />
-                                    <span>Create admin account</span>
-                                </DropdownMenuItem>
-                                <DropdownMenuItem asChild>
-                                    <div className="grid text-xs">
-                                        <span>Email: admin@gmail.com</span>
-                                        <span>Pass: User1234!</span>
-                                    </div>
-                                </DropdownMenuItem>
-                            </DropdownMenuSubContent>
-                        </DropdownMenuSub>
-                    </DropdownMenuGroup>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
+        <>
+            {selected === "add-history" && <HistoryModal
+                open={selected === "add-history"}
+                setOpen={(e) => { if (!e) setSelected("") }}
+            />}
+            <div className="fixed lg:bottom-10 bottom-36 mb-2 right-2">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <Settings2Icon className="absolute h-[1.2rem] w-[1.2rem]" />
+                            <span className="sr-only">Toggle Admin Utils</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>Admin Utilities</DropdownMenuLabel>
+                        <DropdownMenuGroup>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>
+                                    <Users />
+                                    <span>Account</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={handleCreateAdminAccount}>
+                                        <div className="grid gap-1">
+                                            <div className="flex gap-2">
+                                                <UserPlus />
+                                                <span>Create admin account</span>
+                                            </div>
+                                            <div className="grid text-xs">
+                                                <span>Email: admin@gmail.com</span>
+                                                <span>Pass: User1234!</span>
+                                            </div>
+                                        </div>
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                        </DropdownMenuGroup>
+                        <DropdownMenuGroup>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>
+                                    <Users />
+                                    <span>Users</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={handleCreateUserAccount}>
+                                        <div className="grid gap-1">
+                                            <div className="flex gap-2">
+                                                <UserPlus />
+                                                <span>Create user</span>
+                                            </div>
+                                            <div className="grid text-xs">
+                                                <span>Email: user_test@gmail.com</span>
+                                                <span>Pass: User1234!</span>
+                                            </div>
+                                        </div>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem onClick={handleCreateManagmentAccount}>
+                                        <div className="grid gap-1">
+                                            <div className="flex gap-2">
+                                                <UserPlus />
+                                                <span>Create management</span>
+                                            </div>
+                                            <div className="grid text-xs">
+                                                <span>Email: management_test@gmail.com</span>
+                                                <span>Pass: User1234!</span>
+                                            </div>
+                                        </div>
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                        </DropdownMenuGroup>
+                        <DropdownMenuGroup>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>
+                                    <HistoryIcon />
+                                    <span>History</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent>
+                                    <DropdownMenuItem onClick={() => { setSelected("add-history") }}>
+                                        <PlusIcon />
+                                        <span>Add history</span>
+                                    </DropdownMenuItem>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </>
     )
 }
